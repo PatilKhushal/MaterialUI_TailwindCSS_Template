37570a3dc440d90888d2ccd39c46c6a11d8d2f78
PatilKhushal <patilkhushalp2803@gmail.com>
2024-05-22 18:44:50 +0530

    created a template for material UI + tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+const theme = createTheme({
+  components: {
+    MuiPopover: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiPopper: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiDialog: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiModal: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+  },
+});
+
+root.render(
   <React.StrictMode>
-    {"Working"}
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="bg-black text-white ">
+          <Typography variant="h1" textAlign={"center"}>Working</Typography>
+        </div>
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>
-)
+);
